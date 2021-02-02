381c3874632aba74092cd3c08027a367e7325234
Satyaki Bose <satyakibose98@gmail.com>
2021-02-02 17:47:19 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,146 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import questions from "./questions";
 
 function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [dob, setDob] = useState("");
+  const [acceptedTerms, setAcceptedTerms] = useState(false);
+  const [formDisplay, setFormDisplay] = useState(true);
+
+  const [curQuestion, setCurQuestion] = useState(0);
+  const [quizComplete, setQuizComplete] = useState(false);
+  const [showAnswer, setShowAnswer] = useState(false);
+
+  useEffect(() => {
+    setCurQuestion(0);
+    setShowAnswer(false);
+    setQuizComplete(false);
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, email, password, dob);
+    setFormDisplay(false);
+  };
+
+  const handleAnswerOptionClick = (isCorrect) => {
+    setShowAnswer(true);
+  };
+
+  const nextClickHandler = () => {
+    const nextQuestion = curQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurQuestion(nextQuestion);
+    } else {
+      setQuizComplete(true);
+    }
+    setShowAnswer(false);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>online test</h1>
+      {formDisplay ? (
+        <form onSubmit={handleSubmit}>
+          <h2>Create Account</h2>
+          <label>
+            Name:
+            <input
+              name="name"
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </label>
+          <label>
+            Email:
+            <input
+              name="email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </label>
+
+          <label>
+            Password:
+            <input
+              name="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </label>
+          <label>
+            Date of Birth:
+            <input
+              name="dob"
+              type="date"
+              value="2000-07-01"
+              value={dob}
+              onChange={(e) => setDob(e.target.value)}
+            />
+          </label>
+
+          <label>
+            <input
+              name="acceptedTerms"
+              type="checkbox"
+              onChange={(e) => setAcceptedTerms(e.target.value)}
+              required
+            />
+            I accept the terms of service
+          </label>
+
+          <button className="btn">start test</button>
+        </form>
+      ) : (
+        <div className="quiz">
+          {quizComplete ? (
+            <div className="endSection">
+              You have answered all the questions.
+            </div>
+          ) : (
+            <div>
+              {" "}
+              <div className="question-section">
+                <div className="question-count">
+                  Question <span> {curQuestion + 1}</span>/{questions.length}
+                </div>
+                <div className="question-text">
+                  {questions[curQuestion].questionText}
+                </div>
+              </div>
+              <div className="answer-section">
+                {questions[curQuestion].answerOptions.map((answerOption) => {
+                  const optionColor = showAnswer
+                    ? answerOption.isCorrect
+                      ? "green"
+                      : "red"
+                    : null;
+                  return (
+                    <button
+                      className={optionColor}
+                      onClick={() =>
+                        handleAnswerOptionClick(answerOption.isCorrect)
+                      }
+                    >
+                      {answerOption.answerText}
+                    </button>
+                  );
+                })}
+              </div>
+              <button className="nextButton" onClick={nextClickHandler}>
+                Next
+              </button>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/questions.js
@@ -0,0 +1,40 @@
+const questions = [
+  {
+    questionText: "What is the capital of Belgium?",
+    answerOptions: [
+      { answerText: "Paris", isCorrect: false },
+      { answerText: "Oslo", isCorrect: false },
+      { answerText: "Brussels", isCorrect: true },
+      { answerText: "Dublin", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Who is CEO of SpaceX?",
+    answerOptions: [
+      { answerText: "Jeff Bezos", isCorrect: false },
+      { answerText: "Elon Musk", isCorrect: true },
+      { answerText: "Bill Gates", isCorrect: false },
+      { answerText: "Tony Stark", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "The iPhone was created by which company?",
+    answerOptions: [
+      { answerText: "Apple", isCorrect: true },
+      { answerText: "Intel", isCorrect: false },
+      { answerText: "Amazon", isCorrect: false },
+      { answerText: "Microsoft", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "How many Harry Potter books are there?",
+    answerOptions: [
+      { answerText: "1", isCorrect: false },
+      { answerText: "4", isCorrect: false },
+      { answerText: "6", isCorrect: false },
+      { answerText: "7", isCorrect: true },
+    ],
+  },
+];
+
+export default questions;
